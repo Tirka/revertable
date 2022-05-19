a2fafab7090c5e79c97f1c008ec490e34d70d91c
Maksim Vykhota <m.vykhota@protonmail.com>
2022-05-19 03:00:21 +0300

    it builds

--- src/revertable/src/lib.rs
+++ src/revertable/src/lib.rs
@@ -20,25 +20,25 @@ pub fn program(
     accounts: &[AccountInfo],
     _instruction_data: &[u8],
 ) -> ProgramResult {
-    let owner = Pubkey::new_from_array([7; 32]);
-    let lamports = 10_000;
-    // let ether_address = H160([4; 20]);
+    // let owner = Pubkey::new_from_array([7; 32]);
+    // let lamports = 10_000;
+    // // let ether_address = H160([4; 20]);
 
-    let accs = accounts.iter().collect::<Vec<_>>();
-    msg!("accs: {:?}", &accs);
+    // let accs = accounts.iter().collect::<Vec<_>>();
+    // msg!("accs: {:?}", &accs);
     
-    let root = next_account_info(&mut accounts.iter())?;
+    // let root = next_account_info(&mut accounts.iter())?;
 
-    // let instructions = transfer_native_to_evm_ixs(owner, lamports, ether_address);
-    let instructions = vec![];
+    // // let instructions = transfer_native_to_evm_ixs(owner, lamports, ether_address);
+    // let instructions = vec![];
 
-    for (idx, i) in instructions.iter().enumerate() {
-        match invoke(&i, accounts) {
-            Ok(_) => msg!("[{}] ✅ instruction executed successfully: {:?}", idx, &i),
-            Err(program_error) => msg!("[{}] ❎ instruction executed with error: {}", idx, &program_error)
-        }
+    // for (idx, i) in instructions.iter().enumerate() {
+    //     match invoke(&i, accounts) {
+    //         Ok(_) => msg!("[{}] ✅ instruction executed successfully: {:?}", idx, &i),
+    //         Err(program_error) => msg!("[{}] ❎ instruction executed with error: {}", idx, &program_error)
+    //     }
 
-    }
+    // }
 
     Err(ProgramError::Custom(33))
 }
