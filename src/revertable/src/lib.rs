2fc83a15739e36f6179f34cb1a049939568f6792
Maksim Vykhota <m.vykhota@protonmail.com>
2022-05-19 14:52:04 +0300

    compiles

--- src/revertable/src/lib.rs
+++ src/revertable/src/lib.rs
@@ -1,44 +1,154 @@
+use primitive_types::{H160, H256, U256};
+use serde::{Deserialize, Serialize};
 use solana_program::{
-    account_info::{AccountInfo, next_account_info},
+    account_info::{next_account_info, AccountInfo},
     entrypoint,
     entrypoint::ProgramResult,
-    pubkey::Pubkey,
-    program_error::ProgramError,
+    instruction::AccountMeta,
+    instruction::Instruction,
+    msg,
     program::invoke,
-    msg
+    program_error::ProgramError,
+    pubkey::Pubkey,
 };
-
-// use solana_evm_loader_program::{transfer_native_to_evm_ixs};
-
-// use primitive_types::H160;
-// use solana_sdk::transaction::Transaction;
+use std::str::FromStr;
 
 entrypoint!(program);
 
 pub fn program(
-    program_id: &Pubkey,
+    _program_id: &Pubkey,
     accounts: &[AccountInfo],
     _instruction_data: &[u8],
 ) -> ProgramResult {
-    // let owner = Pubkey::new_from_array([7; 32]);
-    // let lamports = 10_000;
-    // // let ether_address = H160([4; 20]);
+    let owner = Pubkey::new_from_array([7; 32]);
+    let lamports = 10_000;
+    let ether_address = H160([4; 20]);
 
-    // let accs = accounts.iter().collect::<Vec<_>>();
-    // msg!("accs: {:?}", &accs);
-    
-    // let root = next_account_info(&mut accounts.iter())?;
+    let accs = accounts.iter().collect::<Vec<_>>();
+    msg!("accs: {:?}", &accs);
 
-    // // let instructions = transfer_native_to_evm_ixs(owner, lamports, ether_address);
-    // let instructions = vec![];
+    let _root = next_account_info(&mut accounts.iter())?;
 
-    // for (idx, i) in instructions.iter().enumerate() {
-    //     match invoke(&i, accounts) {
-    //         Ok(_) => msg!("[{}] ✅ instruction executed successfully: {:?}", idx, &i),
-    //         Err(program_error) => msg!("[{}] ❎ instruction executed with error: {}", idx, &program_error)
-    //     }
+    let instructions = transfer_native_to_evm_ixs(owner, lamports, ether_address);
 
-    // }
+    for (idx, i) in instructions.iter().enumerate() {
+        match invoke(&i, accounts) {
+            Ok(_) => msg!("[{}] ✅ instruction executed successfully: {:?}", idx, &i),
+            Err(program_error) => msg!(
+                "[{}] ❎ instruction executed with error: {}",
+                idx,
+                &program_error
+            ),
+        }
+    }
 
     Err(ProgramError::Custom(33))
+}
+
+fn transfer_native_to_evm_ixs(
+    owner: Pubkey,
+    lamports: u64,
+    ether_address: H160,
+) -> Vec<Instruction> {
+    vec![
+        // FIXME: uncomment
+        // solana_sdk::system_instruction::assign(&owner, &EVM_ID),
+        transfer_native_to_evm(owner, lamports, ether_address),
+        free_ownership(owner),
+    ]
+}
+
+fn free_ownership(owner: Pubkey) -> Instruction {
+    let account_metas = vec![
+        AccountMeta::new(
+            Pubkey::from_str("EvmState11111111111111111111111111111111111").unwrap(),
+            false,
+        ),
+        AccountMeta::new(owner, true),
+    ];
+
+    Instruction::new_with_bincode(
+        Pubkey::from_str("EVM1111111111111111111111111111111111111111").unwrap(),
+        &EvmInstruction::FreeOwnership {},
+        account_metas,
+    )
+}
+
+fn transfer_native_to_evm(owner: Pubkey, lamports: u64, evm_address: H160) -> Instruction {
+    let account_metas = vec![
+        AccountMeta::new(
+            Pubkey::from_str("EvmState11111111111111111111111111111111111").unwrap(),
+            false,
+        ),
+        AccountMeta::new(owner, true),
+    ];
+
+    Instruction::new_with_bincode(
+        Pubkey::default(),
+        &EvmInstruction::SwapNativeToEther {
+            lamports,
+            evm_address,
+        },
+        account_metas,
+    )
+}
+
+#[derive(Debug, PartialEq, Eq, Ord, PartialOrd, Serialize, Deserialize)]
+enum EvmBigTransaction {
+    EvmTransactionAllocate { size: u64 },
+    EvmTransactionWrite { offset: u64, data: Vec<u8> },
+    EvmTransactionExecute {},
+    EvmTransactionExecuteUnsigned { from: H160 },
+}
+
+#[derive(Debug, PartialEq, Eq, Ord, PartialOrd, Serialize, Deserialize)]
+enum EvmInstruction {
+    EvmTransaction {
+        evm_tx: Transaction,
+    },
+    SwapNativeToEther {
+        lamports: u64,
+        evm_address: H160,
+    },
+    FreeOwnership {},
+    EvmBigTransaction(EvmBigTransaction),
+    EvmAuthorizedTransaction {
+        from: H160,
+        unsigned_tx: UnsignedTransaction,
+    },
+}
+
+/// Etherium transaction.
+#[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize)]
+struct Transaction {
+    pub nonce: U256,
+    pub gas_price: U256,
+    pub gas_limit: U256,
+    pub action: TransactionAction,
+    pub value: U256,
+    pub signature: TransactionSignature,
+    pub input: Vec<u8>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+struct UnsignedTransaction {
+    pub nonce: U256,
+    pub gas_price: U256,
+    pub gas_limit: U256,
+    pub action: TransactionAction,
+    pub value: U256,
+    pub input: Vec<u8>,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize)]
+enum TransactionAction {
+    Call(H160),
+    Create,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Serialize, Deserialize)]
+struct TransactionSignature {
+    pub v: u64,
+    pub r: H256,
+    pub s: H256,
 }
