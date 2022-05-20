6585126f41666eef1a1a724cf66d55363b2ec50e
Maksim Vykhota <m.vykhota@protonmail.com>
2022-05-20 13:00:47 +0300

    add env vars

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -1,7 +1,4 @@
-const RPC_URL = 'localhost'
-const KEYPAIR_PATH = '/tmp/keypair.json'
-const PROGRAM_ID = new PublicKey([1, 2, 3])
-
+import dotenv from 'dotenv'
 import fs from 'mz/fs'
 import {
    Connection,
@@ -13,26 +10,37 @@ import {
 } from "@velas/web3"
 
 async function loadKeypair(filePath: string): Promise<Keypair> {
-   const secretKeyString = await fs.readFile(KEYPAIR_PATH, { encoding: 'utf8' })
+   const secretKeyString = await fs.readFile(filePath, { encoding: 'utf8' })
    const secretKey = Uint8Array.from(JSON.parse(secretKeyString))
    return Keypair.fromSecretKey(secretKey)
 }
 
+function callRevertable(user: PublicKey, programId: PublicKey): Transaction {
+   return new Transaction().add(
+      new TransactionInstruction({
+         keys: [
+            { pubkey: user, isSigner: true, isWritable: true }
+         ],
+         programId,
+         data: Buffer.from([0x42]),
+      })
+   )
+}
+
 async function main() {
+   dotenv.config()
+
+   const RPC_URL = process.env.RPC_URL || ''
+   const SIGNER_KEYPAIR = process.env.SIGNER_KEYPAIR || ''
+   const PROGRAM_ID = process.env.PROGRAM_ID || ''
+
+   console.info(`RPC_URL=${RPC_URL}`)
+   console.info(`SIGNER_KEYPAIR=${SIGNER_KEYPAIR}`)
+   console.info(`PROGRAM_ID=${PROGRAM_ID}`)
+
    const connection = new Connection(RPC_URL, 'confirmed')
-
-   const user = await loadKeypair(KEYPAIR_PATH);
-
-   const instruction = new TransactionInstruction({
-      keys: [
-         { pubkey: user.publicKey, isSigner: true, isWritable: true }
-      ],
-      programId: PROGRAM_ID,
-      data: Buffer.from([0x42]),
-   })
-
-   const tx = new Transaction().add(instruction)
-
+   const user = await loadKeypair(SIGNER_KEYPAIR);
+   const tx = callRevertable(user.publicKey, new PublicKey(PROGRAM_ID))
    const result = await sendAndConfirmTransaction(connection, tx, [user])
 
    console.info("client has exited...")
